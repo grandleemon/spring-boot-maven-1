d89e9d743b11514d5d4b3dde80f9e947d424ca58
Max <zeetreex@gmail.com>
2023-02-10 22:03:24 +0200

    getting all customers from the db, changed allocation size for id generation to 1

--- src/main/java/com/grandleemon/Main.java
+++ src/main/java/com/grandleemon/Main.java
@@ -3,11 +3,22 @@ package com.grandleemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1")
 public class Main {
+
+	private final CustomerRepository customerRepository;
+
+	public Main(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
@@ -18,4 +29,9 @@ public class Main {
 	}
 
 	record GreetResponse(String greet){}
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomer() {
+		return customerRepository.findAll();
+	}
 }
