d944ae01e1befc9bcf0aa48150225edce2a1034f
Max <zeetreex@gmail.com>
2023-02-10 22:16:31 +0200

    post request to create a customer and saving him to a db

--- src/main/java/com/grandleemon/Main.java
+++ src/main/java/com/grandleemon/Main.java
@@ -2,9 +2,7 @@ package com.grandleemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,23 @@ public class Main {
 	@GetMapping("/customers")
 	public List<Customer> getCustomer() {
 		return customerRepository.findAll();
+	}
+
+	record NewCustomerRequest(
+					String name,
+					String email,
+					Integer age
+	) {
+
+	}
+
+	@PostMapping("/customers")
+	public void addCustomer(@RequestBody NewCustomerRequest request) {
+		Customer customer = new Customer();
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+
+		customerRepository.save(customer);
 	}
 }
